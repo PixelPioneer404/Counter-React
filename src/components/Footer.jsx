4886b4dbc4f9a37b72a8f2c5d6e5e2938d44604d
PixelPioneer404 <rajbeersaha.me@gmail.com>
2025-08-09 19:58:20 +0530

    fixed footer position

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <>
-            <footer className="absolute bottom-0 left-0 right-0 text-center text-white py-2 sm:py-4 px-4">
+            <footer className="absolute bottom-2 md:bottom-4 left-0 right-0 text-center text-white py-2 sm:py-4 px-4">
                 <p className="text-xs sm:text-sm">© {props.year} - Created with ❤️ by Rajbeer Saha</p>
             </footer>
         </>
